403075760bbf7a1beb53467feade266e84c81b05
Aldo Gutierrez <aldodario1@hotmail.es>
2020-09-13 22:13:00 -0600

    Fixed search engine query

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ $(function () {
 
         if (search.test(input)) {
             // link = conf.search_engine + encodeURIComponent(input.replace(/^:s /g, ""));
-            link = search_engine.replace("search", encodeURIComponent(input.slice(3)));
+            link = conf.search_engine.replace("search", encodeURIComponent(input.slice(3)));
         } else if (site.test(input)) {
             link = input.replace(/^:u /g, "");
         } else {
